475fe9b43ce520ff401db25316bd1c00093a03bd
Fran <fran.p.21@gmail.com>
2022-01-11 00:45:51 -0300

    Update Home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,45 +1,11 @@
 import type { NextPage } from 'next'
 import React from "react"
+import Switch from '../components/Switch'
 
 const Home: NextPage = () => {
-  const [state, setState] = React.useState(true);
-  const handleClick = () => {
-    setState(!state)
-    fetch(`/api/philips/${state.toString()}`)
-  }
+
   return (
-    <div className='container'>
-      <label htmlFor="lightSwitch" className="wrap">
-        <input type="checkbox" checked={!state} onChange={handleClick} id="lightSwitch" name="lightSwitch" className="light-switch" />
-        <div className="panel">
-          <div className="overlay"></div>
-          <div className="screw top">
-            <div className="screw__slot"></div>
-          </div>
-          <div className="switch__hole">
-            <div className="switch__groove">
-              <div className="switch">
-                <div className="switch__shadow-box top">
-                  <div className="switch__shadow top"></div>
-                </div>
-                <div className="switch__shadow-box bottom">
-                  <div className="switch__shadow bottom"></div>
-                </div>
-                <div className="switch__top">
-                  <div className="switch__top-outset"></div>
-                </div>
-                <div className="switch__bottom">
-                  <div className="switch__bottom-outset"></div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="screw bottom">
-            <div className="screw__slot"></div>
-          </div>
-        </div>
-      </label>
-    </div>
+    <div className="container"><Switch /></div>
   )
 }
 
